2aea5e5de67815729b6241f7f1dada2e81caa518
Karlis S <45097959+ks129@users.noreply.github.com>
2019-12-05 20:09:04 +0200

    Update solution.py

--- 2019/day2/solution.py
+++ 2019/day2/solution.py
@@ -7,3 +7,19 @@ data = input.split(',')
 
 #Main proccessor
 def process(in):
+  for i in range(0, len(in), 4):
+    opcode = in[i]
+    val1, val2 = data[data[index + 1]], data[data[index + 1]]
+    if opcode == 99:
+      return data[0]
+    elif opcode == 1:
+      data[data[i + 3]] = val1 + val2
+    elif opcode == 2:
+      data[data[i + 3]] = val1 * val2
+    return data[0]
+  
+#Running
+result = process(data)
+
+#Printing output
+print(f'Result: {result}')
