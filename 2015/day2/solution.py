e759e8185a48ab53040e3cc1cc1a6aed2d46540d
ks129 <45097959+ks129@users.noreply.github.com>
2020-12-04 16:29:47 +0200

    Improve 2015 day 2 solution

--- 2015/day2/solution.py
+++ 2015/day2/solution.py
@@ -1,39 +1,35 @@
-#Getting file name
-print('Insert location and name of input file:')
-file = input()
+import typing as t
 
-#Part 1 Calculations
-def calc1(data):
-    d = data.split('x')
 
-    result = 2 * int(d[0]) * int(d[1]) + 2 * int(d[1]) * int(d[2]) + 2 * int(d[2]) * int(d[0])
-    extra = [int(d[0]) * int(d[1]), int(d[1]) * int(d[2]), int(d[2]) * int(d[0])]
-    extra.sort()
-    result += extra[0]
+def load_input() -> t.List[t.Tuple[int]]:
+    """Load puzzle input in 'input.txt'."""
+    with open('input.txt') as f:
+        return [tuple(map(int, line.split("x"))) for line in f.readlines()]
 
-    return result
 
-#Part 2 Calculations
-def calc2(data):
-    d = data.split('x')
+def part_one(inp: t.List[t.Tuple[int]]) -> int:
+    """
+    Solves part 1 of 2015 day 2 puzzle.
 
-    r1 = 2 * min(int(d[0]) + int(d[1]), int(d[1]) + int(d[2]), int(d[2]) + int(d[0]))
-    r2 = int(d[0]) * int(d[1]) * int(d[2])
-    result = r1 + r2
-    return result
+    Task is calculating amount of wrapping paper. Input is list of package sizes.
+    Formula: 2*l*w + 2*w*h + 2*h*l + smallest side extra.
+    """
+    return sum((2 * l * w) + (2 * w * h) + (2 * h * l) + min((l * w, w * h, h * l)) for l, w, h in inp)
 
-#Creating counters
-result1 = 0
-result2 = 0
 
-#Opening file and looping lines, calcs
-with open(file, 'r') as f:
-    for i in f:
-        res1 = calc1(i)
-        res2 = calc2(i)
-        result1 += res1
-        result2 += res2
+def part_two(inp: t.List[t.Tuple[int]]) -> int:
+    """
+    Solves part 2 of 2015 day 2 puzzle.
 
-#Print results
-print(f'Part 1 result is {result1}')
-print(f'Part 2 result is {result2}')
+    Task is calculating ribbon amount required.
+    Formula: (l + l + w + w) + (l * w * h)
+    """
+    return sum(2 * min(l + w, w + h, h + l) + (l * w * h) for l, w, h in inp)
+
+
+if __name__ == "__main__":
+    data = load_input()
+    print("2015, Day 2, Part 1 solution:")
+    print(part_one(data))
+    print("2015, Day 2, Part 2 solution:")
+    print(part_two(data))
